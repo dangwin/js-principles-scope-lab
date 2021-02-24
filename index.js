eb3d2b64a480fb1b55b5b0efced18a204831c210
dangwin <danghwin@gmail.com>
2021-02-23 19:22:53 -0600

    commit tree c0c0569c23091ebb7ae7a95f50e6a0a2aee4013f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    return customerName 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer
+}
+
+function overwriteBestCustomer(someone){
+    bestCustomer = someone;
+    return bestCustomer
+}
+
+    const leastFavoriteCustomer = 'ME';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'SOMEONE ELSE';
+    return leastFavoriteCustomer
+}
